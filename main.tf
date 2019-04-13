2ed2c911469d3c560914b0d451ed9d440b5962f7
Nuru <Nuru@users.noreply.github.com>
2019-04-12 19:42:34 -0700

    Update to use simpler kops-data-iam

--- main.tf
+++ main.tf
@@ -9,10 +9,8 @@ module "label" {
 }
 
 module "kops_metadata" {
-  source       = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.1.1"
-  dns_zone     = "${var.cluster_name}"
-  masters_name = "${var.masters_name}"
-  nodes_name   = "${var.nodes_name}"
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=data-iam"
+  cluster_name = "${var.cluster_name}"
 }
 
 resource "aws_iam_role" "default" {
